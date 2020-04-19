4bd885dd4d57b6f2a8b19e82ac68fcf561cb37b9
Emily Liu <a62262002@gmail.com>
2020-04-19 17:36:09 +0800

    Add admin travel index page

---
+++ controllers/adminController.js
@@ -0,0 +1,7 @@
+const adminController = {
+  getTravels: (req, res) => {
+    return res.render('admin/travels')
+  }
+}
+
+module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const travelController = require('../controllers/travelController.js')
+const adminController = require('../controllers/adminController.js')
 module.exports = app => {
+  // 前台
   app.get('/', (req, res) => res.redirect('/travels')
   )
   app.get('/travels', travelController.getTravels)
+  // 後台
+  app.get('/admin', (req, res) => res.redirect('/admin/travels'))
+  app.get('/admin/travels', adminController.getTravels)
 }
